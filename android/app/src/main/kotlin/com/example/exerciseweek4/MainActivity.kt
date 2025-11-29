e18b1ed4f7ce7691669c75b3a39fdb00ceb07f4d
Lupo <2224802010717@student.tdmu.edu.vn>
2025-11-29 10:19:33 +0700

    First commit

---
+++ android/app/src/main/kotlin/com/example/exerciseweek4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.exerciseweek4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'shared_prefs_screen.dart';
+import 'async_screen.dart';
+import 'factorial_isolate_screen.dart';
+import 'grid_view_screen.dart';
+import 'list_view_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // Đổi home ở đây để test từng bài
+      // home: const ListViewScreen(),
+      // home: const GridViewScreen(),
+      // home: const SharedPrefsScreen(),
+      // home: const AsyncScreen(),
+     home: const FactorialIsolateScreen(),
+    );
+  }
+}
